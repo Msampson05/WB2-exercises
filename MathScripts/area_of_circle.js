0ed414e640d6c6387c35717650b8d0256880e765
Megan Sampson <msampson@nyc.yearup.org>
2022-10-18 12:24:37 -0400

    updated

--- MathScripts/area_of_circle.js
+++ MathScripts/area_of_circle.js
@@ -1,6 +1,6 @@
 // Area of a circle 
 
 var radius = 6;
-var areaOfCircle = Math.PI * (radius ** 2)
+var areaOfCircle = Math.PI * Math.pow(radius, 2)
 
-console.log("The area of a circle with radius " + radius + "is"  + areaOfCircle)
+console.log(" The area of a circle with radius " + radius + " is "  + areaOfCircle)

371f727dfafab0a8efe34087e91b3a9cc3f0fee4
Megan Sampson <msampson@nyc.yearup.org>
2022-10-19 10:16:32 -0400

    Updated Files

---
+++ WrkBookExcersise/exercise.js
@@ -0,0 +1,12 @@
+// Exercise 1 
+
+var customerId;
+var firstName;
+var middleName;
+var lastName;
+var dateofBirth;
+var driversLicense;
+var autopolicyNumber;
+
+// Ten reserved words you can't use in Java
+

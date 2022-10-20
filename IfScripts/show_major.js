5dcda6cec7023cf00ea33269d2db4170be218fe9
Megan Sampson <msampson@nyc.yearup.org>
2022-10-20 15:47:02 -0400

    Completed show major

---
+++ IfScripts/show_major.js
@@ -0,0 +1,39 @@
+// Major Assignments
+
+  var studentName; 
+  var studentMajor;
+  var location;
+
+  // If else statement 
+
+  if (studentName = " Susan ");
+  {
+    studentMajor = " Biology "
+    location = " Science Bldg, Room 310 "
+    console.log( " Student: " + studentName + "  Major: " + studentMajor + "Advising Location" + location);
+  
+   if ( studentName = " Phan Phan ");
+    
+   studentMajor = " Computer Science "
+   location = " Sheppard Hall, Room 314 "
+   console.log( " Student: " + studentName + "  Major: " + studentMajor + "Advising Location" + location);
+
+   if ( studentName = " Al-Matten ");
+    
+   studentMajor = " English "
+   location = " Kerr Hall, Room 201 "
+   console.log( " Student: " + studentName + "  Major: " + studentMajor + "Advising Location" + location);
+
+   if ( studentName = " Deavion ");
+    
+   studentMajor = " History "
+   location = " Kerr Hall, Room 114 "
+   console.log( " Student: " + studentName + "  Major: " + studentMajor + "Advising Location" + location);
+
+   if ( studentName = " Marketing ");
+    
+   studentMajor = " Computer Science "
+   location = " Westly Hall, Room 310 "
+   console.log( " Student: " + studentName + "  Major: " + studentMajor + "Advising Location" + location);
+}
+  

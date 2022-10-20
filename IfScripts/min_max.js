a59d78e1d73ccc6c7470639e38b68fc2aa1888de
Megan Sampson <msampson@nyc.yearup.org>
2022-10-20 15:17:04 -0400

    completed min and max assignment

---
+++ IfScripts/min_max.js
@@ -0,0 +1,23 @@
+// Numbers 
+   
+  var a = 7;  // Value for smallest number 
+  var b = 8;  // Value for larger number 
+  var c = 9;  // Value for largest number 
+
+  var display; 
+ 
+  //Display the smallest number 
+if (a < b && a < c); 
+{
+  console.log("Smallest Number is " + a );
+  // Display the larger numner
+
+  if ( b > a && b < c);
+
+  console.log("Larger Number is " + b);
+
+  if ( c < b && c < b);
+  
+  console.log("Largest Number is " + c)
+
+}

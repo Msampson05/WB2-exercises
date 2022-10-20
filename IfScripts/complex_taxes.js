d9b1456cc20fb6007e9a08d6471d2076d1920f5c
Megan Sampson <msampson@nyc.yearup.org>
2022-10-20 09:55:59 -0400

    made changes to formula

--- IfScripts/complex_taxes.js
+++ IfScripts/complex_taxes.js
@@ -6,22 +6,56 @@ var grossPay;   // To set value for weekly pay
 var hoursWorked = 45; //Number of hours 
 var payRate= 25.00;  //Hourly pay rate
 var netPay;
-var annualIncomeRange = ((hoursWorked) * (payRate)) * 52 ;
+var annualIncomeRange = (hoursWorked) * (payRate) * 52 ;
 
 var grossPay = (hoursWorked) * ( payRate); // weekly payment
 
-
 if ( filingStatus = "single");
  {
-    taxRate = (15/100)/grossPay;
-    netPay = grossPay - taxRate;
+  if ( annualIncomeRange < 12000)
+  {
+    taxRate = 0.05;
+  } 
+  else if (annualIncomeRange >= 12000 && annualIncomeRange < 25000)
+  {
+    taxRate = 0.10;
+  }
+  else if (annualIncomeRange >= 25000 && annualIncomeRange < 75000)
+  {
+    taxRate = 0.15;
+  }
+  else if (annualIncomeRange> 75000)
+  {
+    taxRate = 0.20;
+  }
+    
+    netPay = annualIncomeRange - taxRate;
  }
  if (filingStatus = "Joint");
- {
+    {
+        if ( annualIncomeRange < 12000)
+        {
+          taxRate = 0.00;
+        } 
 
+        else if (annualIncomeRange >= 12000 && annualIncomeRange < 25000)
+        {
+          taxRate = 0.06;
+        }
+        else if (annualIncomeRange >= 25000 && annualIncomeRange < 75000)
+        {
+          taxRate = 0.11;
+          netPay = annualIncomeRange - taxRate;
+        }
+        else if (annualIncomeRange > 75000)
+        {
+          taxRate = 0.20;
+          netPay = annualIncomeRange - taxRate;
+        }
+       
  }
 
 
 
-console.log("You worked " + hoursWorked + " hours this period. Because you earn  $" + payRate + " per hour, your gross pay is $" + grossPay 
+console.log("You worked " + hoursWorked + " hours this period. Because you earn  $" + payRate + " per hour, your gross pay is $" + annualIncomeRange 
 + " Your filing status is " + filingStatus + " Your tax withholdings this period is " + taxRate + " Your net pay is " + parseInt(netPay))
---
+++ SwitchScripts/dept_converter.js
@@ -0,0 +1,35 @@
+// Switch Excercises
+
+let departmentName = 5;
+let departmentCode;
+switch (departmentName) {
+ case 0:
+ departmentName = "Marketing";
+ departmentCode = 1 ;
+ break;
+ case 1:
+ departmentName = "Human Resources";
+ departmentCode = 5
+ break;
+ case 2:
+ departmentName = "Accounting";
+ departmentCode = 10
+ break;
+ case 3:
+ departmentName = "Legal";
+ departmentCode = 12;
+ break;
+ case 4:
+ departmentName = "IT";
+departmentCode = 18;
+ break;
+ case 5:
+ departmentName = "Customer Relations";
+ departmentCode = 20
+ break;
+ default:
+ dayName = "<unknown>";
+ break;
+}
+
+console.log(departmentName)
